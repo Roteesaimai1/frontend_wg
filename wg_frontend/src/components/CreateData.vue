7e763fd69808d2c33fabcff9f85deb52dc4cf1e7
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-08-03 14:14:38 +0700

    set post data

--- wg_frontend/src/components/CreateData.vue
+++ wg_frontend/src/components/CreateData.vue
@@ -1,36 +1,46 @@
 <template>
     <div class="grid grid-cols-6  gap-3 text-center m-5">  
     <div class="col-start-2 col-span-4  text-sky-50 bg-zinc-900 rounded-md ">  
-      <div class="text-xl text-zinc-50 bg-sky-500 rounded-t-md">เลือกวันที่
+      <!-- <div class="text-xl text-zinc-50 bg-sky-500 rounded-t-md">เลือกวันที่
           <input type="date" class="bg-sky-500 text-sky-50 hover:text-zinc-950" />                              
-      </div>
+      </div> -->
       
       <table class="table-auto w-full">
         <thead >
-          <tr >
-            <th class="bg-sky-500 h-14">ลำดับ</th>
-            <th class="bg-sky-500  h-14">ชื่อ</th>
+          <tr >           
+            <th class="bg-sky-500 rounded-tl-md h-14">ชื่อ</th>
             <th class="bg-sky-500  h-14">สถานะ</th>
             <th class="bg-sky-500  h-14">ยอดเงิน</th>
             <th class="bg-sky-500  h-14">วัน</th>
-            <th class="bg-sky-500  h-14"></th>
+            <th class="bg-sky-500  h-14">วันที่</th>
+            <th class="bg-sky-500 rounded-tr-md h-14"></th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="list in employee" :key="list.id" >
-            <td>{{ list.id }}</td>
-            <td>
-              {{ list.name }} 
-            </td>
-            <td>
-              <select class="py-3 px-4 pr-9 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" v-model="list.selectedStatus">
-                <option disabled value="" selected>เลือกสถานะ</option>
-                <option v-for="item in listmenu" :key="item.id" :value="item.name"> {{ item.name }}</option>                                 
+          <tr>           
+            <td>             
+              <select v-model="datapost.id" id="status" name="status" class=" py-3 px-4 pr-9 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                <option  disabled value="" selected>เลือกชื่อ</option>
+                <option v-for="list in employee" :key="list.id" :value="list.id">{{ list.name }}</option>                                                                               
               </select>
             </td>
-            <td>{{ getPriceBySelectedStatus(list.selectedStatus) }}</td>
-            <td>1</td>
-            <td><button class="bg-sky-500 w-20 h-7 rounded-sm mr-2 hover:bg-sky-300 shadow-md shadow-sky-800">บันทึก</button></td>
+            <td>
+              <select v-model="datapost.status" id="status" name="status" class="py-3 px-4 pr-9 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                <option disabled value="" selected>เลือกสถานะ</option>
+                <option value="DAY">DAY</option>                                 
+                <option value="NIGHT">NIGHT</option>                                 
+              </select>
+            </td>
+            <td>
+              <input type="number" class="w-14 bg-zinc-900 text-center rounded-md"  v-model="datapost.price">
+            </td>
+            <td>
+              <input type="number" class="w-10 bg-zinc-900 rounded-md text-center" v-model="datapost.day">
+            </td>
+            <td>
+              <input type="date" class="py-3 px-4 pr-9 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" />
+            </td>
+            <td><button class="bg-sky-500 w-20 h-7 rounded-sm mr-2 hover:bg-sky-300 shadow-md shadow-sky-800" @click="postData()">บันทึก</button></td>
           </tr>
         </tbody>       
       </table>
@@ -48,17 +58,16 @@ export default {
 
     data() {
         return {
-            selectedStatus: '',
-            listmenu: [
-                { id: 1, name: 'DAY', price: 60},
-                { id: 2, name: 'NIGHT', price: 60},
-                { id: 3, name: 'วันหยุด', price: 0},
-                { id: 4, name: 'ลา', price: 0},
-                { id: 5, name: 'ป่วย', price: 0},
-                { id: 6, name: 'ไม่เลือก', price: 0}
-            ],
+            selectedStatus: '',            
+            daySet: 1,
             employee: [], //Get from api
-             
+            datapost: {
+              id: '',
+              status: '',
+              price: 60,
+              day: 1,
+              date: ''
+            },
             
         }
     }, 
@@ -84,6 +93,9 @@ export default {
             } catch (err) {
               console.error("Error to fetch API :", err);
             }
+        },
+        postData() {
+          alert(`${this.datapost.id} ${this.datapost.status} ${this.datapost.price} ${this.datapost.day} ${this.datapost.date}`)
         },
         resetSelected() {
             window.location.reload();
--- wg_frontend/src/views/SettingPay.vue
+++ wg_frontend/src/views/SettingPay.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="grid grid-cols-3 m-5 gap-5">
+  <div class="grid grid-cols-3 m-5 gap-5 ">
     <div
       class="col-start-1 bg-zinc-800"
       @drop="onDrop($event, 1)"
       @dragenter.prevent
       @dragover.prevent
     >
+    <div class="bg-sky-500 h-10 rounded-t-md">Name</div>
       <div
         v-for="item in getItemsByList(1)"
         :key="item.id"
@@ -13,25 +14,40 @@
         draggable="true"
         @dragstart="stratDrag($event, item)"
       >
-        {{ item.title }}
+        {{ item.title }} -  {{ item.name }}
         <hr>
       </div>
     </div>
     <div
-      class="col-start-2 col-span-2 bg-green-50"
+      class="col-start-2 col-span-2 bg-green-50 bg-zinc-900"
       @drop="onDrop($event, 2)"
       @dragenter.prevent
       @dragover.prevent
     >
+    <!-- POST DATA -->
+    <div class="bg-sky-500 h-10 rounded-t-md">DATE:
+      <input type="date" class="bg-sky-500 text-sky-50 hover:text-zinc-950" v-model="selectedDate" />
+    </div>   
       <div
         v-for="item in getItemsByList(2)"
         :key="item.id"
         class="drag-el text-sky-500 h-10 m-2"
         draggable="true"
         @dragstart="stratDrag($event, item)"
+         
       >
-        {{ item.title }}
-        <input type="radio" />DAY
+        <input type="text">{{ item.title }} -  {{ item.name }}
+        
+        <select class="ml-3 border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" v-model="item.selectStatus">
+            <option disabled value="" selected >เลือกสถานะ</option>
+            <option v-for="status in statuslist" :key="status.id" :value="status.name" >{{ status.name }}</option>                                                                              
+        </select>
+        <span class="ml-5 text-sky-50" >เงินรับ: 
+          <input type="number" class="w-10 bg-zinc-900 text-sky-500" v-model="dataCreate.price"> 
+        </span>       
+        <span class="ml-5 text-sky-50" >วันที่:
+          <input class="bg-zinc-900 text-sky-500" type="date" v-model="dataCreate.date" >
+        </span>       
         <hr>
       </div>
     </div>
@@ -43,10 +59,33 @@ import axios from 'axios'
 export default {
   data() {
     return {     
-      employeelist: []
+      employeelist: [],
+      statuslist: [     
+          { id: 1, name: 'DAY', price: 60},
+          { id: 2, name: 'NIGHT', price: 60},
+          { id: 3, name: 'วันหยุด', price: 0},
+          { id: 4, name: 'ลา', price: 0},
+          { id: 5, name: 'ป่วย', price: 0},
+          { id: 6, name: 'ไม่เลือก', price: 0}
+      ],
+      selectStatus: '',
+      selectedDate: this.getTodayDate(),
+      dataCreate: [{
+        id: '',
+        price: '',
+        day: '',       
+        date: '',
+      }]
     }
   },
-  computed: {},
+  computed: {
+    getPriceBySelectedStatus() {
+            return (selectStatus) => {
+                const selectedStatusItem = this.statuslist.find((item) => item.name === selectStatus);
+                return selectedStatusItem ? selectedStatusItem.price : "";
+      };   
+    },
+  },
   mounted() {
     this.getEmployee()
   },
@@ -55,7 +94,7 @@ export default {
                 return this.employeelist.filter(item => item.list === listId) 
     },  
     stratDrag(event, item) {
-      console.log(item)
+      
       event.dataTransfer.dropEffect = 'move'
       event.dataTransfer.effectAllowed = 'move'
       event.dataTransfer.setData('itemID', item.id)
@@ -79,11 +118,17 @@ export default {
         this.employeelist = employees.map((employee) => ({
             ...employee,
             list: 1,
+            selectStatus: '',
         }));    
       } catch (err) {
         console.error('Error to fetch API :', err)
       }
-    }
+    },
+    getTodayDate() {
+        const today = new Date();
+        today.setHours(today.getHours() + 7);       
+        return today.toISOString().slice(0, 10);
+    },
   }
 }
 </script>
